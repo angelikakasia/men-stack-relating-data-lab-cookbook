7a3493aa482395821bded3843307e2d4ced98bc8
suzanaai <angelska327@gmail.com>
2025-12-11 12:29:46 -0600

    fixed

--- controllers/users.js
+++ controllers/users.js
@@ -6,13 +6,22 @@ const User = require('../models/user.js');
 // SHOW ROUTE - view another user's pantry
 router.get('/', async (req, res) => {
   try {
-    const user = await User.find();
-    res.render('users/show.ejs', { user: user });
+    const users = await User.find();
+    res.render('users/index.ejs', { users: users });
   } catch (err) {
     console.log(err);
     res.redirect('/users');
   }
 });
 
+router.get('/:id', async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    res.render('users/show.ejs', { user: user });
+  } catch (err) {
+    console.log(err);
+    res.redirect('/users');
+  }
+});
 
 module.exports = router;
